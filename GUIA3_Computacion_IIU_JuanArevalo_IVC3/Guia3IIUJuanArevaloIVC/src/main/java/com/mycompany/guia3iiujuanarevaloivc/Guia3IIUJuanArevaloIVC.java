200cbae312547f80177e219b799dbead7b7734ed
2015000097 <j.arevalo.5ddb@gmail.com>
2026-04-24 17:51:53 -0600

    Add files via upload

---
+++ GUIA3_Computacion_IIU_JuanArevalo_IVC3/Guia3IIUJuanArevaloIVC/src/main/java/com/mycompany/guia3iiujuanarevaloivc/Guia3IIUJuanArevaloIVC.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia3iiujuanarevaloivc;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Guia3IIUJuanArevaloIVC {
+
+    public static void main(String[] args) {
+        Mensajes Ej1 = new Mensajes();
+        Ej1.setVisible(true);
+    }
+}
