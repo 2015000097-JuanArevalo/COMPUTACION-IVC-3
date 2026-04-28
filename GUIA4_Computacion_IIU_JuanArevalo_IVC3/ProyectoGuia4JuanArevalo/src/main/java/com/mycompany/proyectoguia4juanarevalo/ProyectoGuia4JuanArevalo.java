ec5722f0ff675279bbaa2ece4798e6c98859a372
2015000097 <j.arevalo.5ddb@gmail.com>
2026-04-27 18:52:21 -0600

    Add files via upload

---
+++ GUIA4_Computacion_IIU_JuanArevalo_IVC3/ProyectoGuia4JuanArevalo/src/main/java/com/mycompany/proyectoguia4juanarevalo/ProyectoGuia4JuanArevalo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoguia4juanarevalo;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class ProyectoGuia4JuanArevalo {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+}
